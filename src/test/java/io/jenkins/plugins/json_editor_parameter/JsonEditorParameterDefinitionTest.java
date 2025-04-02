0da384caba724c2c954e4d17d846580e904cc88a
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-04-02 03:03:06 +0200

    Migrate tests to JUnit5 (#44)
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/io/jenkins/plugins/json_editor_parameter/JsonEditorParameterDefinitionTest.java
+++ src/test/java/io/jenkins/plugins/json_editor_parameter/JsonEditorParameterDefinitionTest.java
@@ -1,49 +1,24 @@
 package io.jenkins.plugins.json_editor_parameter;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import hudson.model.FreeStyleProject;
-import hudson.model.Job;
 import hudson.model.ParametersDefinitionProperty;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import org.htmlunit.html.HtmlElement;
-import org.htmlunit.html.HtmlPage;
-import org.htmlunit.html.HtmlTextInput;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.JenkinsRule.WebClient;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-public class JsonEditorParameterDefinitionTest {
+@WithJenkins
+class JsonEditorParameterDefinitionTest {
     private static final String FIRST_PARAM = "first";
 
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
-
-    /**
-     * Run a build from view with value.
-     *
-     * @param p job to run
-     * @param paramName parameter name to set
-     * @param paramValue parameter value to set
-     * @throws Exception any exceptions
-     */
-    private void runBuildFromView(final Job<?, ?> p, final String paramName, final String paramValue) throws Exception {
-        final WebClient wc = j.createWebClient();
-        wc.setThrowExceptionOnFailingStatusCode(false);
-        final HtmlPage page = wc.getPage(p, "build?delay=0sec");
-        final HtmlElement paramBlock = page.querySelector(String.format("[data-parameter='%s']", paramName));
-        final HtmlTextInput input = paramBlock.getOneHtmlElementByAttribute("input", "name", "value");
-        input.setValueAttribute(paramValue);
-        j.submit(page.getFormByName("parameters"));
-        j.waitUntilNoActivity();
-    }
-
     @Test
-    public void configSimple() throws Exception {
+    void configSimple(JenkinsRule j) throws Exception {
         URL url = getClass().getResource("car.json");
-        Assertions.assertNotNull(url);
+        assertNotNull(url);
 
         JsonEditorParameterDefinition def = new JsonEditorParameterDefinition(FIRST_PARAM);
         def.setSchema(Files.readString(Paths.get(url.toURI())));
